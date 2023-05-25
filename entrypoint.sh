3966ba5e051205f56e673ce0b6ace74e1f9eb803
David Dal Busco <david.dalbusco@outlook.com>
2023-05-25 12:09:01 +0200

    build: create config path if not exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,14 @@ if [ -z "$JUNO_TOKEN" ]; then
 fi
 
 # https://github.com/sindresorhus/env-paths#pathsconfig
-JUNO_LINUX_CONFIG_PATH="$HOME/.config/juno-nodejs/config.json"
+JUNO_LINUX_CONFIG_PATH="$XDG_CONFIG_HOME/juno-nodejs"
+JUNO_LINUX_CONFIG_FILE="$JUNO_LINUX_CONFIG_PATH/config.json"
 
-echo "Storing JUNO_TOKEN in $JUNO_LINUX_CONFIG_PATH"
-echo "{\"token\": $JUNO_TOKEN}" > "$JUNO_LINUX_CONFIG_PATH"
+if [ ! -d "$JUNO_LINUX_CONFIG_PATH" ]; then
+  mkdir "$JUNO_LINUX_CONFIG_PATH"
+fi
+
+echo "Storing JUNO_TOKEN in $JUNO_LINUX_CONFIG_FILE"
+echo "{\"token\": $JUNO_TOKEN}" > "$JUNO_LINUX_CONFIG_FILE"
 
 sh -c "juno $*"
