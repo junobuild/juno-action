0578bcc99cdad39f3d548e30f108bc512e7b2c2d
David Dal Busco <david.dalbusco@outlook.com>
2025-06-17 06:55:29 +0200

    feat: remove JUNO_TOKEN assertion (#30)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ -z "$JUNO_TOKEN" ]; then
-  echo "A JUNO_TOKEN is required to run commands with the Juno cli"
-  exit 126
-fi
-
 if [ -n "$PROJECT_PATH" ]; then
   cd "$PROJECT_PATH"
 fi
