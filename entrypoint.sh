2be917c8f3b3059069d6e730b6cfd3f01ef90809
David Dal Busco <david.dalbusco@outlook.com>
2026-02-13 12:01:00 +0100

    fix: path

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,14 @@ CLEANUP_TOKEN=""
 
 cleanup() {
   if [ -n "$CLEANUP_TOKEN" ]; then
-    (cd ./kit/token && npm run clean 2>&1) || true
+    (cd /kit/token && npm run clean 2>&1) || true
   fi
 }
 
 trap cleanup EXIT
 
 if [ -z "$JUNO_TOKEN" ]; then
-  JUNO_TOKEN=$(cd ./kit/token && npm run auth 2>&1)
+  JUNO_TOKEN=$(cd /kit/token && npm run auth 2>&1)
   echo "::add-mask::$JUNO_TOKEN"
   export JUNO_TOKEN
 
