76f7afa388977099a434dde45cc1524ada593384
David Dal Busco <david.dalbusco@outlook.com>
2026-02-13 15:56:31 +0100

    feat: authenticate and cleanup GH Actions with temporary key

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,35 @@ if [ -n "$PROJECT_PATH" ]; then
   cd "$PROJECT_PATH"
 fi
 
-exec juno "$@" --headless
+CLEANUP_TOKEN=""
+
+cleanup() {
+  if [ -n "$CLEANUP_TOKEN" ]; then
+    (node /kit/token/src/cleanup.ts) || true
+  fi
+}
+
+trap cleanup EXIT
+
+if [ -z "$JUNO_TOKEN" ]; then
+  JUNO_TOKEN=$(node /kit/token/src/authenticate.ts)
+  EXIT_CODE=$?
+
+  case $EXIT_CODE in
+    0)
+      echo "::add-mask::$JUNO_TOKEN"
+      export JUNO_TOKEN
+      CLEANUP_TOKEN="true"
+      ;;
+    1)
+      # An error happened
+      exit 1
+      ;;
+    *)
+      # Skip
+      ;;
+  esac
+fi
+
+juno "$@" --headless
+
