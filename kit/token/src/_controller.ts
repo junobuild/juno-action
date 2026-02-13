647803fc81bf4ad9d65fecdb0ffca05eab6fe7c6
David Dal Busco <david.dalbusco@outlook.com>
2026-02-13 16:09:37 +0100

    feat: generate temporary access keys using GitHub Actions jwt (#72)
    * feat: generate temporary access keys for deployment and publish using GitHub Actions jwt
    
    * feat: review messages
    
    * feat: cleanup
    
    * feat: copy and install automation
    
    * feat: support mode development
    
    * chore: fmt
    
    * feat: cleanup
    
    * chore: fmt
    
    * fix: improvements as tested with mainnet branch
    
    * fix: import
    
    * chore: revert
    
    * feat: more robust

---
+++ kit/token/src/_controller.ts
@@ -0,0 +1,25 @@
+import type {Ed25519KeyIdentity} from '@icp-sdk/core/identity';
+import {deleteSatelliteControllerSelf} from '@junobuild/admin';
+import type {Env} from './_env.ts';
+
+export const deleteController = async ({
+  identity,
+  env: {satelliteId, container}
+}: {
+  identity: Ed25519KeyIdentity;
+  env: Pick<Env, 'satelliteId' | 'container'>;
+}): Promise<{result: 'success'} | {result: 'error'; err: unknown}> => {
+  try {
+    await deleteSatelliteControllerSelf({
+      satellite: {
+        identity,
+        container,
+        satelliteId
+      }
+    });
+
+    return {result: 'success'};
+  } catch (err: unknown) {
+    return {result: 'error', err};
+  }
+};
