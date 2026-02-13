664976322fd40bfd4a8b8ac4b0b2f56d93bbb7e0
David Dal Busco <david.dalbusco@outlook.com>
2026-02-13 06:44:08 +0100

    feat: cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,17 @@ if [ -n "$PROJECT_PATH" ]; then
   cd "$PROJECT_PATH"
 fi
 
+cleanup() {
+  (cd ./kit/token && npm run clean 2>&1) || true
+}
+
+trap cleanup EXIT
+
 if [ -z "$JUNO_TOKEN" ]; then
-  JUNO_TOKEN=$(cd /token && npm run start 2>&1)
+  JUNO_TOKEN=$(cd ./kit/token && npm run start 2>&1)
   echo "::add-mask::$JUNO_TOKEN"
   export JUNO_TOKEN
 fi
 
-# TODO: trap cleanup
-# TODO: docker integration
+juno "$@" --headless
 
-exec juno "$@" --headless
